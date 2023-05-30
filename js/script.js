6e78136b33729583b09a138ed9fdd3d3c13d4b23
Lohan Carvalho <lohan.costa11@gmail.com>
2023-05-29 23:16:09 -0300

    dark | bright mode, aprimoramento de cores e responsividade.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function GetProjects(){ // Função para puxar projetos do meu perfil do github
     const gitHub = 'https://api.github.com/users/lohanjr/repos';
-    var carregando = document.getElementById('loading-id');
+    let carregando = document.getElementById('loading-id');
 
     fetch(gitHub,{
         method: 'GET'
@@ -15,8 +15,31 @@ function GetProjects(){ // Função para puxar projetos do meu perfil do github
     })
 }
 
-function showProjects(data){
+function showProjects(data){ // Função para apropiar nomes, descrições e urls dos projetos vindas da API
+    let unitsProjects = document.getElementById('api-projects');
 
+    for(let i = 0; i < data.length; i++){
+        let a = document.createElement("a");
+        a.href = data[i]['clone_url'];
+        a.target = 'blank_';
+        a.title = data[i]['description'];
+        let nameProject = document.createTextNode(data[i]['name']);
+        a.appendChild(nameProject);
+        unitsProjects.appendChild(a);
+    }
 }
 
 GetProjects();
+
+// Dark / Bright Mode
+
+let themeMode = document.getElementById('icon');
+
+icon.onclick = function(){
+    document.body.classList.toggle("change-theme");
+    if(document.body.classList.contains("change-theme")){
+        themeMode.src = "../imgs/whiteSunTransparent.svg";
+    } else{
+        themeMode.src = "../imgs/moonTransparentFigma.svg";
+    }
+}
